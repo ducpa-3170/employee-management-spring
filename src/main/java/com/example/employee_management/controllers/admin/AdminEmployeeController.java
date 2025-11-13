4a50c51e0ff241e2fa792f12ed5a1902b39c0493
ducpa-3170 <pham.anh.duc-b@sun-asterisk.com>
2025-11-13 13:38:45 +0700

    feat(employee): Add employee

--- src/main/java/com/example/employee_management/controllers/admin/AdminEmployeeController.java
+++ src/main/java/com/example/employee_management/controllers/admin/AdminEmployeeController.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.employee_management.dto.DepartmentDTO;
 import com.example.employee_management.dto.EmployeeDTO;
+import com.example.employee_management.services.DepartmentService;
 import com.example.employee_management.services.EmployeeService;
 
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin/employees")
 public class AdminEmployeeController {
     private final EmployeeService employeeService;
+    private final DepartmentService departmentService;
 
-    public AdminEmployeeController(EmployeeService employeeService) {
+    public AdminEmployeeController(EmployeeService employeeService, DepartmentService departmentService) {
         this.employeeService = employeeService;
+        this.departmentService = departmentService;
     }
 
     @GetMapping
@@ -27,6 +36,40 @@ public class AdminEmployeeController {
         model.addAttribute("employees", employees);
 
         return "pages/employee/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        List<DepartmentDTO> departments = departmentService.getAllDepartments();
+
+        if (!model.containsAttribute("employee")) {
+            model.addAttribute("employee", new EmployeeDTO());
+        }
+
+        model.addAttribute("departments", departments);
+
+        return "pages/employee/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("employee") EmployeeDTO employeeDTO, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        System.out.println(employeeDTO);
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("employee", employeeDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.employee", bindingResult);
+            return "redirect:/admin/employees/create";
+        }
+
+        try {
+            employeeService.create(employeeDTO);
+            redirectAttributes.addFlashAttribute("successMessage", "Employee created successfully!");
+            return "redirect:/admin/employees";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error creating employee: " + e.getMessage());
+            return "redirect:/admin/employees/create";
+        }
     }
 
 }
